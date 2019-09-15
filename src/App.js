75bc456f6a2355167e7a59262602fdcd63ba7638
jensenjamison <jensenjamison@yahoo.com>
2019-09-15 15:02:50 -0500

    finished routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {HashRouter, Link} from "react-router-dom";
+import routes from "./routes"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <nav className="nav-bar">
+        <Link to="/" ><button>Home</button></Link>
+      <div className="nav-right">
+          <Link to="/servers" ><button>Servers</button></Link>
+          <Link><button>Kits</button></Link>
+          <Link><button>Contact</button></Link>
+        </div>
+      </nav>
+      {routes}
+    </HashRouter>
   );
 }
 
---
+++ src/components/Servers.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+
+
+export default class Servers extends Component{
+    constructor(){
+        super();
+        this.state={
+
+        }
+
+    }
+    render(){
+        return(
+            <div>
+                servers
+            </div>
+        )
+    }
+
+}
